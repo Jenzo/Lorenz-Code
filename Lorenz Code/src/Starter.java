4389c84c4b128f889da4c999d52416498b8f0bd8
jenzo <jeschr1986@gmail.com>
2021-10-14 12:58:13 +0200

    First commit

---
+++ Lorenz Code/src/Starter.java
@@ -0,0 +1,106 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Starter {
+
+	private static Map<Character, String> murrayCode = new HashMap<>();
+	private static final int WORD_LENGTH = 5;
+
+	public static void main(String[] args) {
+		initMurrayMap();
+
+		String plaintext = "ABCDE";
+		String key = "01010 10010 01010 10001 01010";
+		String xorCipher = xor(encodePlaintext(plaintext), key);
+		System.out.println("decrypted ciphertext: " + decodeCiphertext(xor(xorCipher, key)));
+
+	}
+
+	private static String encodePlaintext(String p) {
+		Objects.requireNonNull(p, "Plantext must not be null at this point.");
+		StringBuilder sb = new StringBuilder();
+		p = p.toUpperCase();
+		for (int i = 0; i < p.length(); ++i) {
+			sb.append(encode(p.charAt(i)));
+			if (i < p.length() - 1) {
+				sb.append(" ");
+			}
+		}
+
+		return sb.toString();
+	}
+
+	private static String decodeCiphertext(String c) {
+		Objects.requireNonNull(c, "Ciphertext must not be null at this point.");
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < c.length(); i += WORD_LENGTH + 1) {
+			sb.append(decode(c.substring(i, i + WORD_LENGTH)));
+		}
+		return sb.toString();
+	}
+
+	private static String xor(String a, String b) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < a.length(); ++i) {
+			sb.append(xorBitwise(a.charAt(i), b.charAt(i)));
+		}
+		return sb.toString();
+	}
+
+	private static String encode(char key) {
+		String result = murrayCode.get(key);
+		if (result == null) {
+			System.out.println(String.format("Key '%s' is not mapped results in null", key));
+		}
+
+		return result;
+	}
+
+	private static char decode(String value) {
+		for (Map.Entry<Character, String> e : murrayCode.entrySet()) {
+			if (e.getValue().equals(value)) {
+				return e.getKey();
+			}
+		}
+		throw new IllegalArgumentException(String.format("Value '%s' is not mapped", value));
+	}
+
+	private static char xorBitwise(char a, char b) {
+		if (a == ' ' || b == ' ') {
+			return ' ';
+		}
+		return a == b ? '0' : '1';
+	}
+
+	private static void initMurrayMap() {
+		// Letters
+		murrayCode.putIfAbsent('A', "00011");
+		murrayCode.putIfAbsent('B', "11001");
+		murrayCode.putIfAbsent('C', "01110");
+		murrayCode.putIfAbsent('D', "01001");
+		murrayCode.putIfAbsent('E', "00001");
+		murrayCode.putIfAbsent('F', "01101");
+		murrayCode.putIfAbsent('G', "11010");
+		murrayCode.putIfAbsent('H', "10100");
+		murrayCode.putIfAbsent('I', "00110");
+		murrayCode.putIfAbsent('J', "01011");
+		murrayCode.putIfAbsent('K', "01111");
+		murrayCode.putIfAbsent('L', "10010");
+		murrayCode.putIfAbsent('M', "11100");
+		murrayCode.putIfAbsent('N', "01100");
+		murrayCode.putIfAbsent('O', "11000");
+		murrayCode.putIfAbsent('P', "10110");
+		murrayCode.putIfAbsent('Q', "10111");
+		murrayCode.putIfAbsent('R', "01010");
+		murrayCode.putIfAbsent('S', "00101");
+		murrayCode.putIfAbsent('T', "10000");
+		murrayCode.putIfAbsent('U', "00111");
+		murrayCode.putIfAbsent('V', "11110");
+		murrayCode.putIfAbsent('W', "10011");
+		murrayCode.putIfAbsent('X', "11101");
+		murrayCode.putIfAbsent('Y', "10101");
+		murrayCode.putIfAbsent('Z', "10001");
+	}
+
+}
